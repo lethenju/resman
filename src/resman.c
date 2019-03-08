cb0632ad43b71ec742f82261600cad3612026746
Julien LE THENO <julien.letheno@gmail.com>
2019-03-08 09:14:22 +0100

    [Fix] Various typos and wrong names

--- src/resman.c
+++ src/resman.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "resman_types.h"
 #include "resman.h"
 
@@ -7,7 +8,7 @@ static resman_ctx *context;
 void resman_init()
 {
     context = malloc(sizeof(resman_ctx));
-    memset(context, NULL, sizeof(resman_ctx));
+    memset(context, 0, sizeof(resman_ctx));
     context->current_id = 0;
 }
 
@@ -17,12 +18,12 @@ void *resman_alloc(char *name, int size)
     while (res != NULL)
         res = res->next;
     res = malloc(sizeof(resman_resource));
-    memset(res, NULL, sizeof(resman_resource));
-    res->resource_id = ctx->current_id;
+    memset(res, 0, sizeof(resman_resource));
+    res->resource_id = context->current_id;
     res->resource_name = name;
     res->nb_bytes = size;
     res->ptr_to_res = malloc(sizeof(size));
-    ctx->current_id++;
+    context->current_id++;
     return res->ptr_to_res;
 }
 
@@ -31,9 +32,9 @@ void resman_free(void *resource)
     resman_resource *res = context->list;
     if (res->ptr_to_res == resource)
     {
-        resman_resource *new_first = context->list->next 
+        resman_resource *new_first = context->list->next ;
         free(context->list->ptr_to_res);
-        free(context->list->name);
+        free(context->list->resource_name);
         free(context->list);
         context->list = new_first;
     } 
@@ -44,7 +45,7 @@ void resman_free(void *resource)
         // we have to remove the element in the middle of the queue.
         // so we have to free the unused element
         free (res->next->ptr_to_res);
-        free (res->next->resource_name)
+        free (res->next->resource_name);
         free (res->next);
         // then set the before element's next on the next directly, 
         res->next = res->next->next;
@@ -58,7 +59,7 @@ void resman_end()
     {
         // free all resources
         resman_resource *next = res->next;
-        free(res->name);
+        free(res->resource_name);
         free(res->ptr_to_res);
         free(res);
         res = next; 
