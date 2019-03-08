63f77b439a16b80de4969fc018c6d6e601574102
Julien LE THENO <julien.letheno@gmail.com>
2019-03-08 09:19:51 +0100

    [Doc] Functions documentation

--- inc/resman.h
+++ inc/resman.h
@@ -1,12 +1,24 @@
 #ifndef RESMAN_H
 #define RESMAN_H
 
+/** Inits the resource manager system
+ */
 void resman_init();
 
+/** Replaces your malloc call
+ *  @param name : Give a name to your allocation to better keep track of it
+ *  @param size : The size of your allocation, in bytes
+ *  @returns void* : A pointer to your allocated memory
+ */
 void* resman_alloc(char* name, int size);
 
+/** Replaces your free call
+ *  @param resource : a pointer to your resource
+ */
 void resman_free(void* resource);
 
+/** Ends the resource manager and frees all resources allocated with it.
+ */
 void resman_end();
 
 #endif
