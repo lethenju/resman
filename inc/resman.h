55f06c6159bd5dc39fead9046e311a9ec9e848c3
Julien LE THENO <julien.letheno@gmail.com>
2019-03-18 09:59:04 +0100

    [Feat] Add getters and debug functions

--- inc/resman.h
+++ inc/resman.h
@@ -17,8 +17,41 @@ void* resman_alloc(char* name, int size);
  */
 void resman_free(void* resource);
 
+/** Prints the allocated resources for debug purposes
+ */ 
+void resman_print_resources();
+
+/** Returns the number of resources allocated by resman
+ *  @return int : the number of resources allocated by resman
+ */  
+int resman_get_number_of_resources();
+
+/** Gets the id of a given resource name
+ *  @param name : the name of the resource 
+ *  @returns int : the id of the resource
+ */ 
+int resman_get_id_of_resource(char* name);
+
+/** Gets the name of a given resource id
+ *  @param id : the id of the resource
+ *  @returns char* : the name of the resource
+ */ 
+char* resman_get_name_of_resource(int id);
+
+/** Gets the size of a resource in bytes
+ *  @param id : the id of the resource
+ *  @returns int : the size of the resource in bytes
+ */ 
+int resman_get_size_of_resource(int id);
+
+/** Gets the total allocated size at this moment
+ *  @returns the total allocated size in bytes
+ */ 
+int resman_get_total_size_allocated();
+
 /** Ends the resource manager and frees all resources allocated with it.
  */
 void resman_end();
+
 
 #endif
